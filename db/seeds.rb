d7aa1ca1be2a48921e4871b82ee3a274eb9d898d
Jonas Wahl <jonasjorge97@gmail.com>
2024-05-21 17:46:15 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+puts 'Creating 10 fake articles...'
+Article.destroy_all
+10.times do
+  Article.create!(
+    title: Faker::ChuckNorris.fact,
+    content: Faker::GreekPhilosophers.quote
+  )
+end
+puts 'Finished!'
